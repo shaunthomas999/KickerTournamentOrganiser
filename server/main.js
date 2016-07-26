c58fe7e7ccb2c6a975903312f23f4eed535546df
Shaun Thomas <thomas@innoscale.de>
2016-07-27 01:18:29 +0200

    Made provision to add some test data on application startup

--- server/main.js
+++ server/main.js
@@ -1,5 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import '../api/participants.js';
+import '../api/fixturesNScores.js';
+
+import { ParticipantsCollection } from '../api/participants.js';
+import { FixturesAndScoresCollection } from '../api/fixturesNScores.js';
 
 /*
  * This method will be executed at the startup of application
@@ -7,4 +11,77 @@ import '../api/participants.js';
  * */
 Meteor.startup(() => {
   console.log("=== Starting Kicker Tournament Organiser ===");
+
+  // Clear ParticipantsCollection
+  ParticipantsCollection.remove({});
+
+  // Clear FixturesAndScoresCollection
+  FixturesAndScoresCollection.remove({});
+
+  let fakeParticipants = [{"name" : "Adam Berry",
+      "nickname" : "Adam",
+      "emailAddress" : "adamberry@gmail.com",
+      "score": 0
+    },
+    {
+      "name" : "Benjamin Black",
+      "nickname" : "Ben",
+      "emailAddress" : "bblack@gmail.com",
+      "score": 0
+    },
+    {
+      "name" : "Frank Davidson",
+      "nickname" : "Frank",
+      "emailAddress" : "davidson@hotmail.com",
+      "score": 0
+    },
+    {
+      "name" : "Steven Fisher",
+      "nickname" : "Steve",
+      "emailAddress" : "s_fisher@yahoo.com",
+      "score": 0
+    },
+    {
+      "name" : "Amelia Butler",
+      "nickname" : "Amy",
+      "emailAddress" : "amybutler@gmail.com",
+      "score": 0
+    },
+    {
+      "name" : "Elizabeth Glover",
+      "nickname" : "Liz",
+      "emailAddress" : "glover_liz@yahoo.com",
+      "score": 0
+    },
+    {
+      "name" : "Jane Hudson",
+      "nickname" : "Jan",
+      "emailAddress" : "hubson@web.de",
+      "score": 0
+    },
+    {
+      "name" : "Katherine McDonald",
+      "nickname" : "Kathy",
+      "emailAddress" : "k4kathy@gmail.com",
+      "score": 0
+    },
+    {
+      "name" : "Natalie Ross",
+      "nickname" : "Rossy",
+      "emailAddress" : "natalier@gmail.com",
+      "score": 0
+    },
+    {
+      "name" : "Sebastian Thomson",
+      "nickname" : "Sebu",
+      "emailAddress" : "sebu@web.de",
+      "score": 0
+    }
+  ];
+
+  for(let index in fakeParticipants) {
+    console.log("Inserting: " + fakeParticipants[index].nickname);
+    ParticipantsCollection.insert(fakeParticipants[index]);
+  }
+
 });
